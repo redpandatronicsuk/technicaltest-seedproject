ec5ede12b189c3c3d6f64d2a4e78658969c27606
RedPandaTronics <dobe@Dominiks-MacBook-Pro-2.local>
2017-06-01 19:42:05 +0100

    Moved shared constants to separate folder

---
+++ decision-tech-react-seed/src/data/constants.js
@@ -0,0 +1,10 @@
+// By default no filter is selected
+const defaultProductTypeFilterSelections = {
+  Broadband: false,
+  TV: false,
+  Mobile: false
+}
+
+export {
+    defaultProductTypeFilterSelections
+}
