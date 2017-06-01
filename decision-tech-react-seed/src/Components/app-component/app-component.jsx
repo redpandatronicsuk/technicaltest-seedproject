6123ed58edad3b41f5ae2aa2ae4e6ad091b49fca
RedPandaTronics <dobe@Dominiks-MacBook-Pro-2.local>
2017-06-01 18:32:47 +0100

    Clean up

--- decision-tech-react-seed/src/Components/app-component/app-component.jsx
+++ decision-tech-react-seed/src/Components/app-component/app-component.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { filterDealsByProductTypesAndSpeed } from '../../util'
 var service = function () {
   return {
     fetchDeals: () => {
@@ -9,17 +10,17 @@ var service = function () {
   }
 }();
 
+// By default no filter is selected
+const defaultProductTypeFilterSelections = {
+  Broadband: false,
+  TV: false,
+  Mobile: false
+}
+
 const filterOptions = {
-  // To make it more extensible, we could use objects instead
-  // such as: {displayName: 'Broadband', filterValue: 'Broadband', ...}
-  // but it seems the display value is the same as the raw data in the
-  // JSON, so I opted for compactness
-  type: [
-    'Broadband',
-    'TV',
-    'Mobile'
-  ],
+  type: Object.keys(defaultProductTypeFilterSelections),
   speed: [
+    'ANY',
     76,
     52,
     17
@@ -32,7 +33,8 @@ export default class App extends React.Component {
     this.state = {
       'deals': [],
       menuOpen: false,
-      selectedBroadbandFilterTypes: {}
+      selectedBroadbandFilterTypes: defaultProductTypeFilterSelections,
+      selectedSpeed: 'ANY'
     };
     service
       .fetchDeals()
@@ -45,6 +47,7 @@ export default class App extends React.Component {
       menuOpen: !this.state.menuOpen
     })
   }
+
   render() {
     return (
       <div>
@@ -65,15 +68,14 @@ export default class App extends React.Component {
               d="M4,10h24c1.104,0,2-0.896,2-2s-0.896-2-2-2H4C2.896,6,2,6.896,2,8S2.896,10,4,10z M28,14H4c-1.104,0-2,0.896-2,2  s0.896,2,2,2h24c1.104,0,2-0.896,2-2S29.104,14,28,14z M28,22H4c-1.104,0-2,0.896-2,2s0.896,2,2,2h24c1.104,0,2-0.896,2-2  S29.104,22,28,22z"/>
           </svg>
         </div>
-        {/*{JSON.stringify(this.state.deals)}*/}
-        {/* Though I could use id as key as they should be unique, but there is at least one duplicate */}
-        {this.state.deals.map((deal, i) => <div key={i} className="deal ">
+        {/* Thought I could use id as key as they should be unique, but there is at least one duplicate */}
+        {filterDealsByProductTypesAndSpeed(this.state.deals, this.state.selectedBroadbandFilterTypes, this.state.selectedSpeed === 'ANY' ? null : this.state.selectedSpeed).map((deal, i) => <div key={i} className="deal ">
           <div>{deal.title}</div>
           <div>{deal.contractLength}months</div>
           <div>{deal.speed.label} MB</div>
           <div>{deal.usage.label}</div>
           <img src={deal.offer.smallLogo} alt={deal.offer.title} title={deal.offer.title} />
-          {deal.popularChannels ? deal.popularChannels.map(channel => <img src={channel.logo} alt={channel.name} title={channel.name} />) : null}
+          {deal.popularChannels ? deal.popularChannels.map((channel, i) => <img key={i} src={channel.logo} alt={channel.name} title={channel.name} />) : null}
           {deal.mobile ? <div>
               <div>Data: {deal.mobile.data.label}</div>
               <div>Minutes: {deal.mobile.minutes.label}</div>
@@ -91,7 +93,7 @@ export default class App extends React.Component {
               {filterOptions.type.map(broadbandType => <li key={broadbandType}>
                 <input 
                 type="checkbox"
-                value={broadbandType}
+                checked={this.state.selectedBroadbandFilterTypes[broadbandType]}
                 onChange={()=> {
                   const { selectedBroadbandFilterTypes } = this.state
                   selectedBroadbandFilterTypes[broadbandType] = !selectedBroadbandFilterTypes[broadbandType]
@@ -101,8 +103,8 @@ export default class App extends React.Component {
               )}
             </ul>
             <div>Speed</div>
-            <select value={this.state.value} onChange={this.handleChange}>
-              {filterOptions.speed.map(speed => <option value={speed}>{speed}</option>)}
+            <select value={this.state.selectedSpeed} onChange={(event) => this.setState({selectedSpeed: event.target.value})}>
+              {filterOptions.speed.map(speed => <option key={speed} value={speed}>{speed}</option>)}
             </select>
           </div>
       </div>
